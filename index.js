71d54ef842f0f714e6a37ba22e876d6abd0923db
ShinHyungJune <ssa4141@naver.com>
2019-11-15 18:26:01 +0900

    add real domain

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let event = null;
 let replies = [];
 
 let localDomain = "http://localhost:80";
+let realDomain = "https://craw.in-diary.com";
 
 const craw = async () => {
 	const browser = await puppeteer.launch({headless: false, args: ["--window-size=1920,1080", '--disable-notifications']});
@@ -61,7 +62,7 @@ const craw = async () => {
 };
 
 
-axios.get(localDomain + '/api/events', {
+axios.get(realDomain + '/api/events', {
 	params: {
 		take: 1,
 		orderBy: "created_at",

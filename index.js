904023a6d459fd0fae42ae2792b67e3ecadb504b
ShinHyungJune <ssa4141@naver.com>
2019-11-15 18:24:15 +0900

    refactoring try catch

--- index.js
+++ index.js
@@ -10,60 +10,55 @@ let localDomain = "http://localhost:80";
 let realDomain = "https://craw.in-diary.com";
 
 const craw = async () => {
-	try{
-		const browser = await puppeteer.launch({headless: false, args: ["--window-size=1920,1080", '--disable-notifications']});
-		const page = await browser.newPage();
-		const getReplies = async () => {
-			replies = await page.$$eval('div.C4VMK', replies => replies.map((reply) => {
-				const nickname = reply.querySelector('a.FPmhX').textContent;
-				const link = reply.querySelector("a.FPmhX").href;
-				const body = reply.querySelector('span').textContent;
-				const repliedAt = reply.querySelector('time').title;
-
-				return {nickname, link, body, replied_at: repliedAt};
-			}));
-		};
-
-
-		await page.setViewport({
-			width:1000,
-			height:1080
-		});
-
-		await page.goto(event.link);
-
-		let btnMore = await page.$('ul.XQXOT button.dCJp8'); // 더보기 버튼
-
-		if(!btnMore){
-			await getReplies();
-		}else{
-			while(btnMore){
-				btnMore.click();
-
-				try{
-					btnMore = await page.waitForSelector('ul.XQXOT button.dCJp8');
-				}catch(e){
-					btnMore = null;
-
-					await getReplies();
-				}
-			}
+	const browser = await puppeteer.launch({headless: false, args: ["--window-size=1920,1080", '--disable-notifications']});
+	const page = await browser.newPage();
+	const getReplies = async () => {
+		replies = await page.$$eval('div.C4VMK', replies => replies.map((reply) => {
+			const nickname = reply.querySelector('a.FPmhX').textContent;
+			const link = reply.querySelector("a.FPmhX").href;
+			const body = reply.querySelector('span').textContent;
+			const repliedAt = reply.querySelector('time').title;
+			
+			return {nickname, link, body, replied_at: repliedAt};
+		}));
+	};
+	
+	
+	await page.setViewport({
+		width:1000,
+		height:1080
+	});
+	
+	await page.goto(event.link);
+	
+	let btnMore = await page.$('ul.XQXOT button.dCJp8'); // 더보기 버튼
+	
+	if(!btnMore){
+		await getReplies();
+	}else{
+		while(btnMore){
+			btnMore.click();
+			
+			btnMore = await page.waitForSelector('ul.XQXOT button.dCJp8').catch((error) => {
+				console.log(error);
+				
+				btnMore = null;
+				
+				getReplies();
+			});
 		}
-
-		await axios.post(localDomain + '/api/replies', {
-			"event_id" : event.id,
-			"replies" : replies
-		}).then((response) => {
-			console.log(response.data);
-		});
-
-		await page.close();
-
-		await browser.close();
 	}
-	catch(e){
-		console.log(e);
-	}
+	
+	await axios.post(localDomain + '/api/replies', {
+		"event_id" : event.id,
+		"replies" : replies
+	}).then((response) => {
+		console.log(response.data);
+	});
+	
+	await page.close();
+	
+	await browser.close();
 };
 
 
@@ -76,7 +71,9 @@ axios.get(localDomain + '/api/events', {
 }).then((response) => {
 	event = response.data.data[0];
 
-	craw();
+	craw().catch((error) => {
+		console.log(error);
+	});
 }).catch((error) => {
 	console.log(error);
 });
